986f35eb00845ff2b80ba14345dd09dd9800e84f
ebrahimstb <azeezhibro006@gmail.com>
2023-04-24 02:27:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import {Banner} from './component/Banner';
  import { NavBar } from "./component/NavBar";
  import 'bootstrap/dist/css/bootstrap.min.css';
 import { Skills } from './component/Skills';
+import { Projects } from './component/Projects';
+import { Contact } from "./component/Contact"
+import { Footer } from './component/Footer';
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <NavBar/>
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
